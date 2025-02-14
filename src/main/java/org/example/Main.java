c938e7ac07ef2b683a4002a91708e1a7136e2538
JoWi235 <jonas.wilmar23@gmail.com>
2025-02-14 21:44:50 +0100

    erster schritt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome! \n");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-
+        Scanner scanner = new Scanner(System.in);
+        try {
+            /*int firstNum = scanner.nextInt();
+            System.out.println(firstNum);*/
+            System.out.println(scanner.nextInt()+scanner.nextInt());
+        } catch (Exception e) {
+            System.out.println("Falsche Eingabe");
         }
-        System.out.println("Wie beginne ich??");
     }
 }
